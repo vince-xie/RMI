16171eaf3300a63deedca215489efd282915a821
Edmond Wu <edma_wu@yahoo.com>
2016-10-05 16:56:28 -0400

    began client implementation

--- RMI/src/client/Client.java
+++ RMI/src/client/Client.java
@@ -1,10 +1,16 @@
 package client;
 
+import java.rmi.Naming;
+import java.io.*;
+
 /**
  * @author Vincent Xie, Edmond Wu
  */
 public class Client {
     public static void main(String[] args) {
-
+    	int port = 1099;
+    	int argLength = args.length;
+    	String city = args[argLength - 2];
+    	String state = args[argLength - 1];
     }
 }
