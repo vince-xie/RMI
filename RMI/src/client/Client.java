01123bd23a424227dc7a27393ab4e4e6f704f6ef
Edmond Wu <edma_wu@yahoo.com>
2016-10-06 13:43:56 -0400

    command line parsing attempt

--- RMI/src/client/Client.java
+++ RMI/src/client/Client.java
@@ -2,15 +2,29 @@ package client;
 
 import java.rmi.Naming;
 import java.io.*;
+import java.util.*;
 
 /**
  * @author Vincent Xie, Edmond Wu
  */
 public class Client {
     public static void main(String[] args) {
+    	for (String s : args) {
+    		System.out.println(s);
+    	}
     	int port = 1099;
+    	String server = "localhost";
     	int argLength = args.length;
     	String city = args[argLength - 2];
     	String state = args[argLength - 1];
+    	List<String> arguments = Arrays.asList(args);
+    	if (arguments.contains("-h")) {
+    		server = arguments.get(arguments.indexOf("-h") + 1);
+    	}
+    	if (arguments.contains("-p")) {
+    		port = Integer.parseInt(arguments.get(arguments.indexOf("-p") + 1));
+    	}
+    	String url = "//" + server + ":" + port;
+    	System.out.println("City: " + city + ", State: " + state + ", Server: " + server + ", Port: " + port);
     }
 }
